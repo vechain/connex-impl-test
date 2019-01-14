a99a4c62988b27d58b32fce1a640c81a89d15152
tony <tony.li@vechain.com>
2019-01-14 10:18:21 +0800

    optimize source map

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ module.exports = {
     lintOnSave: false,
     baseUrl: './',
     configureWebpack: {
-        devtool: 'cheap-module-eval-source-map' // For mocha to display code properly
+        devtool: 'eval' // For mocha to display code properly
     }
 }
